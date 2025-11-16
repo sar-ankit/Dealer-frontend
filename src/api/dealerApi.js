d7908ab5606f508439c74b28e9a0b79cd18360dc
sar-ankit <akr083632@gmail.com>
2025-11-16 18:36:14 +0530

    chore: add axios dependency

--- src/api/dealerApi.js
+++ src/api/dealerApi.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-const BASE_API_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-const BASE_URL = `${BASE_API_URL}/dealer`;
-
+const BASE_URL = 'https://veldel.up.railway.app/api/dealer';
 
 
 
